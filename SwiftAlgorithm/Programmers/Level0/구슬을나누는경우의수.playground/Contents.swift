041ab334d36e93fe10d6551ad4dda722a32d391d
zoop1259 <kdm432191@gmail.com>
2022-11-21 23:13:37 +0900

    ing..

--- SwiftAlgorithm/Programmers/Level0/구슬을나누는경우의수.playground/Contents.swift
+++ SwiftAlgorithm/Programmers/Level0/구슬을나누는경우의수.playground/Contents.swift
@@ -5,11 +5,11 @@ let share = 3
 
 // n! / (n-m)! * m!
 
-var b = 1
-var s = 1
+var b = 1.0
+var s = 1.0
 var subs = balls - share
-var sub = 1
-var res = 0
+var sub = 1.0
+var res = 0.0
 
 if subs <= 1 {
     print(1)
@@ -18,20 +18,54 @@ if subs <= 1 {
 
 
 for i in 1...balls {
-    b = b * i
+    b = b * Double(i)
     print(b)
 }
 
 for i in 1...share {
-    s = s * i
+    s = s * Double(i)
 }
 
 for i in 1...subs {
-    sub = sub * i
+    sub = sub * Double(i)
 }
 
 
 res = b / (sub * s)
 
-//왜 논리적 오류가 발생하는가. 문제 오류인지 모르겠다.
+//왜 논리적 오류가 발생하는가. 문제 오류인지 모르겠다. //28번이 안된다.
+import Foundation
+
+func solution(_ balls:Int, _ share:Int) -> Int {
+var b = 1.0
+var s = 1.0
+var subs = Double(balls - share)
+var sub = 1.0
+
+if subs < 1 {
+    return 1
+} else if share == 0 {
+    return 0
+} else if balls == share {
+    return 1
+} else if balls == 1 {
+    return 1
+} else if balls == 0 {
+    return 0
+}
+
+for i in 1...balls {
+    b = b * Double(i)
+}
+
+for i in 1...share {
+    s = s * Double(i)
+}
+
+for i in 1...balls - share {
+    sub = sub * Double(i)
+}
+    
+    return Int(round(b / (sub * s)))
+}
 
--- SwiftAlgorithm/Programmers/Level0/외계어사전.playground/Contents.swift
+++ SwiftAlgorithm/Programmers/Level0/외계어사전.playground/Contents.swift
@@ -1,8 +1,79 @@
 import UIKit
 
+//제출한답.
+import Foundation
+
+func solution(_ spell:[String], _ dic:[String]) -> Int {
+
+    var setB = Set(spell)  //셋으로 만들어주고
+
+    var arr : [[String]] = [] //2차원 빈배열 생성해주고.
+
+    for i in stride(from: 0, to: dic.count, by: 1) { //2차원 배열 안의 1차원배열에 값을 넣어주고.
+        arr.append(dic[i].map{ String($0)})
+    }
+    
+    var res = [Int]() //결과값을 위한 배열을 선언해주고.
+    
+    for i in 0..<arr.count {
+        var setA = Set(arr[i]) //1차원 배열들을 set으로 만들어주고.
+        if setA.intersection(setB).count == spell.count { //요소가 겹치는지 확인.
+            res.append(1)
+        } else {
+            res.append(2)
+        }
+    }
+    
+    if res.contains(1) == true { //배열안에 1이 하나라도 있으면 리턴1
+        return 1
+    } else {
+        return 2
+    }
+    
+    return 0
+}
+//한줄코딩... 제한사항을 확실하게 준수하신듯.
+//하나하나 해보자.
+func solution2(_ spell: [String], _ dic: [String]) -> Int { dic.map { String($0.sorted()) }.contains(spell.sorted().joined()) ? 1 : 2 }
+
+
+
+/* 삽질들...
 let spell = ["p", "o", "s"]
 //let dic = ["sod", "eocd", "qixm", "adio", "soo"]
 let dic = ["podu", "eocd", "qixm", "adio", "soo"]
+
+var setB = Set(spell)
+
+var arr : [[String]] = []
+
+for i in stride(from: 0, to: dic.count, by: 1) {
+    arr.append(dic[i].map{ String($0)})
+}
+var a = [String]()
+for i in 0..<arr.count {
+    
+    var setA = Set(arr[i])
+    
+    if setA.intersection(setB).count == spell.count {
+        print("오오")
+    } else {
+        print("아니아니")
+    }
+    
+}
+
+var setB = Set(spell)
+//var setA = Set(dic)
+//
+//for i in dic {
+//    if setA.isSubset(of: setB) {
+//        print("오오")
+//    } else {
+//        print("아니아니")
+//    }
+//}
+
 let dicMap = dic[1].map{$0}
 print(dicMap)
 var copySpell = spell
@@ -14,20 +85,28 @@ for i in stride(from: 0, to: dic.count, by: 1) {
 }
 print(arr)
 
+var setArr = Set(arr)
+
 var a = 0
 //print(arr.count)
 for i in 0..<arr.count {
     //print("ㅁㄴㅇ a : \(a)")
     //a = 0
     
+    var setA = Set(arr[i])
+    
+    if setA.isSubset(of: setB) {
+        print("오오")
+    } else {
+        print("아니아니")
+    }
+    
+    
     for j in 0..<arr[i].count {
 //        print("\(arr[i][j])")
         //a += 1
         //print("여기 a : \(a)")
     }
-    
-    
-    
 }
             //2차원배열생성         //1차원에 뭘 담을지,      //몇개를 담을지,  2차원 배열 개수
 //var arr = [[String]](repeating: Array(repeating: <#T##_#>, count: <#T##Int#>), count: <#T##Int#>)
@@ -72,3 +151,5 @@ for i in 0...asd.count - 1 {
     
 }
 print(zxc)
+
+*/
