6fd9264c4ae609a391acd5bfd027367a5fd38f49
zoop1259 <kdm432191@gmail.com>
2023-01-10 22:38:14 +0900

    ingg..

---
+++ SwiftAlgorithm/Programmers/Level2/튜플.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+//집합에서 "가장 빈번히 나온 숫자" 순으로 튜플이 구성됩니다
+print(123)
+func solution(_ s:String) -> [Int] {
+    var arr = [Int]()
+    let tu = s.components(separatedBy: ",").map { $0 }
+    print(tu)
+
+    for str in tu {
+        var strs = ""
+        for cha in str {
+            if cha.isNumber {
+                strs += String(cha)
+            }
+        }
+        arr.append(Int(strs)!)
+    }
+
+    var countDictionary = [Int: Int]()
+    for element in arr {
+        countDictionary[element] = (countDictionary[element] ?? 0) + 1
+    }
+    let sortedArr = countDictionary.sorted { $0.value > $1.value }.map { $0.key }
+    
+    return sortedArr
+}
+
+//solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
+solution("{{20,111},{111}}")
