72b797d3b2f629d4e3476499f60ca4c822949c7e
zoop1259 <kdm43211991@gmail.com>
2023-03-23 22:58:59 +0900

    ing...

--- SwiftAlgorithm/Programmers/Level2/모음사전.playground/Contents.swift
+++ SwiftAlgorithm/Programmers/Level2/모음사전.playground/Contents.swift
@@ -1,11 +1,13 @@
 import Foundation
 
+let vowelsDic = ["A":0,"E":1,"I":2,"O":3,"U":4]
+let count = [781,156,31,6,1]
 
 func solution(_ word:String) -> Int {
     let vowelsDic = ["A":0,"E":1,"I":2,"O":3,"U":4]
     let count = [781,156,31,6,1]
 
-    return word.enumerated().map{countDic[$0.offset]*vowelsDic[String($0.element)]!}.reduce(word.count) { $0 + $1}
+    return word.enumerated().map{count[$0.offset]*vowelsDic[String($0.element)]!}.reduce(word.count) {$0 + $1}
 }
 
 solution("AAAAI")
@@ -64,4 +66,30 @@ A E I    A E I   A E I A E I
  
  
  
+ */
+
+/*
+ //아래코드는 dfs인데 완전탐색이라 시간이 많이 걸린다.
+ var wordDict = [String:Int]()
+ var count = 0
+
+ func solution(_ word:String) -> Int {
+     DFS(wordLetters: [])
+     return wordDict[word]!
+ }
+
+ func DFS(wordLetters: [String]) {
+     let word = wordLetters.joined()
+     wordDict[word] = count
+     
+     if wordLetters.count == 5 {
+         return
+     } else {
+         let letters = ["A", "E", "I", "O", "U"]
+         for letter in letters {
+             count += 1
+             DFS(wordLetters: wordLetters + [letter])
+         }
+     }
+ }
  */
